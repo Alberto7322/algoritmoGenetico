4beedb794059595d13d7874056b133bc229425ba
100429155 <100429155@alumnos.uc3m.es>
2022-09-16 14:33:03 +0200

    v3

--- inicializacion.py
+++ inicializacion.py
@@ -17,23 +17,47 @@ def iniciar(m, n):
 
 def evaluar(poblacion):
     resultados = []
-    mejor = 10000000000
-    pos_elegido = None
+    pos_elegido = []
     for i in poblacion:
             cromosoma = i
             web = "http://memento.evannai.inf.uc3m.es/age/test?c="
             r = requests.get(web + cromosoma)
             resultados.append(r.text)
-    for i in range(10):
-        x = random.randint(0, len(resultados))
-        elegido = float(resultados[x])
-        if elegido < mejor:
-            mejor = elegido
-            pos_elegido = x
-    print(mejor)
+
+    for k in range(6):
+        mejor = 10000000000
+        for i in range(10):
+            x = random.randint(0, len(resultados))
+            elegido = float(resultados[x])
+            if elegido < mejor:
+                mejor = elegido
+                pos_elegido.append(x)
+        print(mejor)
     return pos_elegido
 
 poblacion = iniciar(m, n)
 print(poblacion)
 mejor = evaluar(poblacion)
-print(poblacion[mejor])
+for i in range(6):
+    print(poblacion[mejor[i]])
+
+def cruce(cromosoma1,cromosoma2):
+    hijo1=""
+    hijo2=""
+    for i in range(len(cromosoma1)):
+        a=cromosoma1[i]
+        b=cromosoma2[i]
+        r=random.randint(0,1)
+        if r==0:
+            hijo1+=a
+        else:
+            hijo1+=b
+        s=random.randint(0,1)
+        if s==0:
+            hijo2+=a
+        else:
+            hijo2+=b
+    return hijo1 +"\n"+hijo2
+
+
+
