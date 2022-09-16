4a1c2732814d83b34dcf48eb8413b7aa42a3ea03
100429155 <100429155@alumnos.uc3m.es>
2022-09-16 13:55:06 +0200

    v1

--- inicializacion.py
+++ inicializacion.py
@@ -1,8 +1,13 @@
 import random
-m=100
-n=32
-poblacion=[]
+m = 100
+n = 32
+poblacion = []
 for i in range(m):
+    poblacion.append([])
     for j in range(n):
-        poblacion[i][j]=random.randint(0,1)
-print(poblacion)
+        poblacion[i].append(random.randint(0,1))
+
+for i in poblacion:
+    print(i,"\n")
+
+print(len(poblacion))
