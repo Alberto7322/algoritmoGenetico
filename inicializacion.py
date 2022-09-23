9317d2ae81e40758520e56999dfc9ff817dc42af
100429155 <100429155@alumnos.uc3m.es>
2022-09-23 14:36:10 +0200

    v4 23/09/2022

--- inicializacion.py
+++ inicializacion.py
@@ -24,52 +24,75 @@ def evaluar(poblacion):
             resultados.append(r.text)
     return resultados
 
-def seleccionar(resultados):
+def seleccionar(resultados, m, poblacion):
     pos_elegido = []
-    for k in range(6):
+    for k in range(m):
         mejor = 10000000000
-        for i in range(10):
-            x = random.randint(0, len(resultados))
+        for i in range(4):
+            x = random.randint(0, len(resultados)-1)
             elegido = float(resultados[x])
             if elegido < mejor:
                 mejor = elegido
-        print(mejor)
-        pos_elegido.append(x)
+        #print(mejor)
+        pos_elegido.append(poblacion[x])
 
     return pos_elegido
 
-poblacion = iniciar(m, n)
-resultados = evaluar(poblacion)
-mejor = seleccionar(resultados)
-print(mejor)
-"""for i in range(6):
-    print(poblacion[mejor[i]])
-"""
-def cruce(cromosoma1,cromosoma2):
-    hijo1 = ""
-    hijo2 = ""
-    for i in range(len(cromosoma1)):
-        a = cromosoma1[i]
-        b = cromosoma2[i]
-        r = random.randint(0, 1)
-        if r == 0:
-            hijo1 += a
-        else:
-            hijo1 += b
-        s = random.randint(0, 1)
-        if s == 0:
-            hijo2 += a
-        else:
-            hijo2 += b
-    return hijo1 + "\n" + hijo2
+def cruce(mejores):
 
-def mutacion (hijos):
-    for i in range(len(hijos)):
-        for j in range(len(hijos[i])):
+    poblacion = []
+
+    for i in range(0, len(mejores), 2):
+        hijo1 = ""
+        hijo2 = ""
+        padre = mejores[i-1]
+        madre = mejores[i]
+
+        for j in range(len(padre)):
+            a = padre[j]
+            b = madre[j]
+            r = random.randint(0, 1)
+            if r == 0:
+                hijo1 += a
+            else:
+                hijo1 += b
+            s = random.randint(0, 1)
+            if s == 0:
+                hijo2 += a
+            else:
+                hijo2 += b
+        poblacion.append(hijo1)
+        poblacion.append(hijo2)
+    return poblacion
+
+def mutacion (poblacion):
+    poblacion2 = []
+    for i in range(len(poblacion)):
+        aux = ""
+        for j in range(len(poblacion[i])):
             aleatorio = random.randint(0, 100)
             if aleatorio <= 5:
-                if hijos[i][j] == 0:
-                    hijos[i][j] = 1
+                if poblacion[i][j] == "0":
+                    aux += "1"
                 else:
-                    hijos[i][j] = 0
-    return hijos
+                    aux += "0"
+            else:
+                aux +=poblacion[i][j]
+        poblacion2.append(aux)
+    return poblacion2
+
+poblacion = iniciar(m, n)
+for k in range(150):
+    resultados = evaluar(poblacion)
+    mejores = seleccionar(resultados, m, poblacion)
+    cruzados = cruce(mejores)
+    poblacion = mutacion(cruzados)
+    resultados_f = evaluar(poblacion)
+    mejor = 1000000000000
+    for i in range(len(resultados_f)):
+        comparado = float(resultados_f[i])
+        if comparado < mejor:
+            mejor = comparado
+            pos = i
+
+    print("El mejor resultado es:", mejor, "del cromosoma", poblacion[pos], "en la iteracion", k)
