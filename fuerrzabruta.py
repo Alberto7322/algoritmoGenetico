463e58a8807cf009d1e5cfa1a332bf72c4c785d4
100429119 <100429119@alumnos.uc3m.es>
2022-10-06 11:03:05 +0200

    FB

--- fuerrzabruta.py
+++ fuerrzabruta.py
@@ -1,1 +1,27 @@
+import requests
+import random
 
+cromosoma = ""
+for i in range(100):
+    cromosoma += str(random.randint(0,1))
+
+
+while r != 0:
+    aux = ""
+    for i in cromosoma:
+        cambio = random.randint(0, 50)
+        if cambio <= 5:
+            if i == "1":
+                aux += 0
+            else:
+                aux += "1"
+        else:
+            aux += i
+
+    web = "http://memento.evannai.inf.uc3m.es/age/test?c="
+    ra = requests.get(web + aux)
+    r = requests.get(web + cromosoma)
+    if ra < r:
+        r = ra
+        cromosoma = aux
+    print(r.text)
