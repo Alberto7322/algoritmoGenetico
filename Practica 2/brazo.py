df961fc65186949b9ec5348d47474112ad9df8e1
100429155 <100429155@alumnos.uc3m.es>
2022-10-21 14:23:17 +0200

    cruzamiento nuevo

---
+++ Practica 2/brazo.py
@@ -0,0 +1,45 @@
+import requests
+import random
+from random import uniform, gauss
+import time
+print(random.randrange(0,1,1))
+
+
+extension = 4
+
+def incializar():
+    padre = []
+    varianzas = []
+    for _ in range(extension):
+        padre.append(random.uniform(-180, 180))
+        varianzas.append(random.uniform(100, 300))
+    return padre, varianzas
+
+def mutar(padre, varianzas):
+    hijo = []
+    for i in range(len(padre)):
+        var = padre[i] + random.gauss(0, varianzas[i])
+        hijo.append(var)
+
+    return hijo
+
+
+def evaluar(padre):
+    web = "http://memento.evannai.inf.uc3m.es/age/robot4?"
+    for i in range(len(padre)):
+        if i != extension -1:
+            c = "c"+str(i+1)+"="+str(padre[i])+"&"
+        else:
+            c = "c"+str(i+1)+"=" + str(padre[i])
+        web = web + c
+    r = requests.get(web)
+    return r.text
+
+def seleccion(ev_padre, ev_hijo):
+
+
+    pass
+print(evaluar([0,0,0,0]))
+
+
+
